10f8617287e67536f4dc6b68241cdb0cf64047a6
lucianoZgabriel <lucianozanin@gmail.com>
2024-11-05 10:15:10 -0400

    chore: add tests for minting

--- test/AuraToken.test.ts
+++ test/AuraToken.test.ts
@@ -1,4 +1,7 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import {
+  loadFixture,
+  time,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 
@@ -103,5 +106,72 @@ describe("AuraToken", function () {
         .connect(otherAccount)
         .transferFrom(owner.address, otherAccount.address, 1001n * 10n ** 18n)
     ).to.be.revertedWithCustomError(auraToken, "ERC20InsufficientBalance");
+  });
+
+  it("Should mint once", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    const mintAmount = await auraToken.setMintAmount(100n * 10n ** 18n);
+    await auraToken.mint();
+    const totalSupply = await auraToken.totalSupply();
+    const ownerBalance = await auraToken.balanceOf(owner.address);
+    expect(totalSupply).to.equal(1100n * 10n ** 18n);
+    expect(ownerBalance).to.equal(1100n * 10n ** 18n);
+  });
+
+  it("Should mint twice (different acounts)", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    await auraToken.setMintAmount(100n * 10n ** 18n);
+    await auraToken.mint();
+
+    const instance = auraToken.connect(otherAccount);
+    await instance.mint();
+    const totalSupply = await auraToken.totalSupply();
+    const ownerBalance = await auraToken.balanceOf(owner.address);
+    const otherAccountBalance = await auraToken.balanceOf(otherAccount.address);
+    expect(totalSupply).to.equal(1200n * 10n ** 18n);
+    expect(ownerBalance).to.equal(1100n * 10n ** 18n);
+    expect(otherAccountBalance).to.equal(100n * 10n ** 18n);
+  });
+
+  it("Should mint twice (_mintDelay)", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    await auraToken.setMintAmount(100n * 10n ** 18n);
+    await auraToken.mint();
+    await time.increase(60 * 60 * 24 * 2);
+    await auraToken.mint();
+    const totalSupply = await auraToken.totalSupply();
+    const ownerBalance = await auraToken.balanceOf(owner.address);
+    expect(totalSupply).to.equal(1200n * 10n ** 18n);
+    expect(ownerBalance).to.equal(1200n * 10n ** 18n);
+  });
+
+  it("Should NOT set mint amount (not owner)", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    const instance = auraToken.connect(otherAccount);
+    expect(instance.setMintAmount(100n * 10n ** 18n)).to.be.rejectedWith(
+      "AuraToken: only owner can call this function"
+    );
+  });
+
+  it("Should NOT set mint delay (not owner)", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    const instance = auraToken.connect(otherAccount);
+    expect(instance.setMintDelay(60 * 60 * 24 * 2)).to.be.rejectedWith(
+      "AuraToken: only owner can call this function"
+    );
+  });
+
+  it("Should NOT mint", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    expect(auraToken.mint()).to.be.rejectedWith("AuraToken: mint amount is 0");
+  });
+
+  it("Should NOT mint twice (mint delay not passed)", async function () {
+    const { auraToken, owner, otherAccount } = await loadFixture(deployFixture);
+    await auraToken.setMintAmount(100n * 10n ** 18n);
+    await auraToken.mint();
+    expect(auraToken.mint()).to.be.rejectedWith(
+      "AuraToken: mint is not allowed yet"
+    );
   });
 });
