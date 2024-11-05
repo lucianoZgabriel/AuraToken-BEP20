42c1c2ad1a42861307fc164199140afe2268bbe5
lucianoZgabriel <lucianozanin@gmail.com>
2024-11-05 09:44:45 -0400

    feat: add minting functionality

--- contracts/AuraToken.sol
+++ contracts/AuraToken.sol
@@ -4,7 +4,40 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AuraToken is ERC20 {
+    address private _owner;
+    uint256 private _mintAmount = 0;
+    uint256 private _mintDelay = 60 * 60 * 24; // 1 day
+
+    mapping(address => uint256) private _netxMintTime;
+
     constructor() ERC20("AuraToken", "AUR") {
+        _owner = msg.sender;
         _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+
+    function mint() public {
+        require(_mintAmount > 0, "AuraToken: mint amount is 0");
+        require(
+            _netxMintTime[msg.sender] < block.timestamp,
+            "AuraToken: mint is not allowed yet"
+        );
+        _mint(msg.sender, _mintAmount);
+        _netxMintTime[msg.sender] = block.timestamp + _mintDelay;
+    }
+
+    function setMintAmount(uint amount) public onlyOwner {
+        _mintAmount = amount;
+    }
+
+    function setMintDelay(uint256 delayInSeconds) public onlyOwner {
+        _mintDelay = delayInSeconds;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == _owner,
+            "AuraToken: only owner can call this function"
+        );
+        _;
     }
 }
